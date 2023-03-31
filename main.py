c6f543cfdb7683f196f142caf6715258fa4f512a
L0g_ <90014937+L0g123@users.noreply.github.com>
2023-03-31 14:08:17 +1300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+
         self.setWindowTitle("audioussy")
-        button = QPushButton("press me")
+
+        button = QPushButton("press for gay sex")
+        button.setCheckable(True)
+
+        button.clicked.connect(self.the_button_was_clicked)
+
         self.setFixedSize(QSize(400, 300))
         self.setCentralWidget(button)
+    def the_button_was_clicked(self):
+        print("not yet ;)")
+
+
+
 
 app = QApplication(sys.argv)
 
